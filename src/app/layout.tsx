1d5e9eff849a1c57f4f958319d538f6c1b9bce69
matheus <dev.matheuspc@gmail.com>
2025-10-13 15:16:18 -0300

    inserido google analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Script from 'next/script';
 
 // 1. Importe o CSS e o Container do react-toastify
 import { ToastContainer } from 'react-toastify';
@@ -27,6 +28,8 @@ export const metadata: Metadata = {
   title: "Consulta de Situação - CPNU",
   description: "Consulte os detalhes da sua classificação no Concurso Público Nacional Unificado.",
 };
+
+const GA_MEASUREMENT_ID = process.env.NEXT_PUBLIC_GA_ID;
 
 export default function RootLayout({
   children,
@@ -57,6 +60,23 @@ export default function RootLayout({
           theme="dark" // Perfeito para o seu design
         />
       </body>
+     {/* 2. Adicione os scripts do Google Analytics aqui, fora do body principal, mas dentro do <html> */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `,
+        }}
+      />
     </html>
   );
 }
